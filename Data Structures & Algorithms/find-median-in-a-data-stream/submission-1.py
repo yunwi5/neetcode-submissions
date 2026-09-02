1172f66e51abcd378465d09645e4e334144bda3b
Yunkeun <86972879+yunwi5@users.noreply.github.com>
2026-09-03 03:30:36 +1200

    Add: find-median-in-a-data-stream - submission-1

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-1.py
@@ -0,0 +1,38 @@
+class MedianFinder:
+
+    def __init__(self):
+        self.firstHalfHeap = [] # max heap
+        self.secondHalfHeap = [] # min heap
+
+    def addNum(self, num: int) -> None:
+        if not self.firstHalfHeap:
+            self.firstHalfHeap.append(-num)
+            return
+        
+        # Add to correct heap
+        if num < -self.firstHalfHeap[0]:
+            heapq.heappush(self.firstHalfHeap, -num)
+        else:
+            heapq.heappush(self.secondHalfHeap, num)
+        
+        # Calibration
+        totalSize = len(self.firstHalfHeap) + len(self.secondHalfHeap)
+        maxAllowedSize = math.ceil(totalSize / 2)
+        if len(self.firstHalfHeap) > maxAllowedSize:
+            poppedNum = -heapq.heappop(self.firstHalfHeap)
+            heapq.heappush(self.secondHalfHeap, poppedNum)
+        if len(self.secondHalfHeap) > maxAllowedSize:
+            poppedNum = -heapq.heappop(self.secondHalfHeap)
+            heapq.heappush(self.firstHalfHeap, poppedNum)
+
+    def findMedian(self) -> float:
+        totalSize = len(self.firstHalfHeap) + len(self.secondHalfHeap)
+        # Even
+        if totalSize % 2 == 0:
+            return (-self.firstHalfHeap[0] + self.secondHalfHeap[0]) / 2
+        # Odd
+        if len(self.firstHalfHeap) == totalSize//2 + 1:
+            return -self.firstHalfHeap[0]
+
+        return self.secondHalfHeap[0]       
+        
