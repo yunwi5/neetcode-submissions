7e49534aadaed565341687a323f9021bc2452bf7
Yunkeun <86972879+yunwi5@users.noreply.github.com>
2026-09-02 18:51:44 +1200

    Add: task-scheduling - submission-1

---
+++ Data Structures & Algorithms/task-scheduling/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        # Time: O(m)
+        # Space: O(1)
+        freqDict = collections.Counter(tasks)
+
+        total = 0
+        while freqDict:
+            maxHeap = []
+            for task, freq in freqDict.items():
+                heapq.heappush(maxHeap, (-freq, task))
+
+            heapSize = len(maxHeap)
+            for i in range(min(heapSize, n + 1)):
+                # Highest count task in the heap at iteration i
+                _, task = heapq.heappop(maxHeap)
+                freqDict[task] -= 1
+                if freqDict[task] < 1:
+                    del freqDict[task]
+
+            if not freqDict:
+                total += heapSize
+                break
+            total += (n + 1)
+
+        return total
