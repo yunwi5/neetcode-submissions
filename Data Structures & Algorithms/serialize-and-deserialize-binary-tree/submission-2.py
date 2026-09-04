2aef9f1d11907b85e599e4d20eed4a60f59bb3db
Yunkeun <86972879+yunwi5@users.noreply.github.com>
2026-09-04 17:05:25 +1200

    Add: serialize-and-deserialize-binary-tree - submission-2

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-2.py
@@ -0,0 +1,81 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Codec:
+    
+    # Encodes a tree to a single string.
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        # preorder traversal
+        # make it a full tree with nulls as child.
+
+        def dfs(node: Optional[TreeNode]) -> int:
+            if not node:
+                return 0
+            
+            leftHeight = dfs(node.left)
+            rightHeight = dfs(node.right)
+
+            return 1 + max(leftHeight, rightHeight)
+        
+        treeHeight = dfs(root)
+        listSize = 2**(treeHeight + 1) - 1
+
+        result = []
+        q = deque([(root, 0)])
+        level = 0
+        while q:
+            qLen = len(q)
+
+            for i in range(qLen):
+                node, nodeIndex = q.popleft()
+                if not node:
+                    continue
+                result.append('|'.join([str(node.val), str(nodeIndex)]))
+
+                if node.left:
+                    leftIndex = 2 * nodeIndex + 1
+                    q.append((node.left, leftIndex))
+                if node.right:
+                    rightIndex = 2 * nodeIndex + 2
+                    q.append((node.right, rightIndex))
+
+        return ",".join(result)
+
+        
+    # Decodes your encoded data to tree.
+    def deserialize(self, data: str) -> Optional[TreeNode]:
+        bfsList = data.split(',')
+        if not bfsList or bfsList[0] == '':
+            return None
+        
+        nodeMapByIndex = {}
+        for nodePair in bfsList:
+            nodeVal, nodeIndex = nodePair.split('|')
+            nodeMapByIndex[int(nodeIndex)] = int(nodeVal)
+
+        rootVal = nodeMapByIndex[0]
+        root = TreeNode(int(rootVal))
+        q = deque([(root, 0)])
+
+        while q:
+            node, bfsIndex = q.popleft()
+            leftIndex = bfsIndex * 2 + 1
+            rightIndex = bfsIndex * 2 + 2
+
+            if leftIndex in nodeMapByIndex:
+                leftVal = nodeMapByIndex[leftIndex]
+                leftNode = TreeNode(leftVal)
+                node.left = leftNode
+                q.append((leftNode, leftIndex))
+            if rightIndex in nodeMapByIndex:
+                rightVal = nodeMapByIndex[rightIndex]
+                rightNode = TreeNode(int(rightVal))
+                node.right = rightNode
+                q.append((rightNode, rightIndex))
+            
+        return root
+
