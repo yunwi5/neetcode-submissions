ec9a0eb6fd5f98045cb36fe852adfd84903b5fe9
Yunkeun <86972879+yunwi5@users.noreply.github.com>
2026-09-12 19:11:33 +1200

    Add: design-word-search-data-structure - submission-2

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-2.py
@@ -0,0 +1,58 @@
+class WordDictionary:
+
+    def __init__(self):
+        self.root = Node()
+
+    def addWord(self, word: str) -> None:
+        # Time: O(n)
+        # Space: O(t + n)
+        # n = length of string, t = # nodes
+
+        cur = self.root
+        for c in word:
+            if cur.hasChild(c):
+                cur = cur.getChild(c)
+            else:
+                newChild = Node()
+                cur.children[c] = newChild
+                cur = newChild
+        cur.word = True
+
+    def search(self, word: str) -> bool:
+        # Time: O(26 * 26 * n)
+        # Space: O(t + n)
+
+        # A A
+        # B *
+        # C
+        # D D
+        cands = [self.root]
+        for c in word:
+            newCands = []
+            for cand in cands:
+                if c == '.':
+                    newCands.extend(list(cand.children.values()))
+                else:
+                    child = cand.getChild(c)
+                    if child is not None:
+                        newCands.append(child)
+            cands = newCands
+        
+        return any(cand.word for cand in cands)
+        
+class Node:
+
+    def __init__(self):
+        self.children = {}
+        self.word = False
+    
+    def hasChild(self, c: str):
+        return c in self.children
+
+    def getChild(self, c: str):
+        if c in self.children:
+            return self.children[c]
+    
+    def __repr__(self):
+        return f'Node(children={list(self.children.keys())})'
+        
