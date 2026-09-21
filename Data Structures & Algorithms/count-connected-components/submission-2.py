2965b6b19c29382715642bf01d39af4ec24ba085
Yunkeun <86972879+yunwi5@users.noreply.github.com>
2026-09-21 21:55:45 +1200

    Add: count-connected-components - submission-2

---
+++ Data Structures & Algorithms/count-connected-components/submission-2.py
@@ -0,0 +1,57 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        # Build adjacency list
+        # o
+        #
+        # Use DFS?
+        # Time: O(V+E)
+        # Space: O(V+E)
+        # V = # vertices, E = # edges
+    
+
+        connectedComponents = 0
+        # Time: O(V)
+        # Space: O(V)
+        unvisited = {i for i in range(n)}
+        adjList = {}
+
+        # Time: O(E)
+        # Space: O(E)
+        for edge in edges:
+            if edge[0] not in adjList:
+                adjList[edge[0]] = set()
+            if edge[1] not in adjList:
+                adjList[edge[1]] = set()
+
+            adjList[edge[0]].add(edge[1])
+            adjList[edge[1]].add(edge[0])
+        
+
+        # Time: O(V+E)
+        # Space: O(V+E)
+        def dfs(node: int, component):
+            component.add(node)
+            if node not in adjList:
+                return 
+
+            neighs = adjList[node]
+            for neigh in neighs:
+                if neigh in component:
+                    continue
+                if neigh in unvisited:
+                    unvisited.remove(neigh)
+                dfs(neigh, component)
+
+        while unvisited:
+            node = unvisited.pop()
+            connectedComponents += 1
+            component = set()
+            dfs(node, component)
+
+        return connectedComponents
+
+        
+
+        
+
+        
